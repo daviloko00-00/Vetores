65ab4b04c2a23d6a4612532e1e4d537f0293c23d
unknown <davicustodiosilva36@gmail.com>
2025-05-19 16:06:29 -0300

    Contagem de idades, código finalizado

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-let idades = [];// variável para idades declarada.
+let idades = [];
+let posicao = [];// variável para idades declarada.
 
 
-for (i = 0; i < 3; i++) {
+for (i = 0; i < 4; i++) {
     let idade;
     do {
         idade = parseFloat(prompt(`Digite a idade ${i+1}`))
-        if (isNaN(idade) || idade < 0 || idade > 100) {
+        if (isNaN(idade) || idade < 0 ) {
             alert("Idade inválida, digite novamente");
         }
-    } while (isNaN(idade) || idade < 0 || idade > 100);
+    } while (isNaN(idade) || idade < 0 );
+    idades[i] = idade
 }
+let maiorIdade = idades[0];
+let posicaoMaiorIdade = 0;
 
-
+for (let i = 0; i < 4; i++) {
+    if (idades[i] > maiorIdade) {
+        maiorIdade = idades[i];
+        posicaoMaiorIdade = i; // Aqui salva a posição do maior valor
+    }
+}
+alert(`A maior idade é = ${maiorIdade}, posição ${posicaoMaiorIdade}° lugar`);
