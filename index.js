a19a02b36e14575e081b0d92aaaa7bac208f986a
unknown <davicustodiosilva36@gmail.com>
2025-05-19 15:34:06 -0300

    Contagem de idades

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let idades = [];// variável para idades declarada.
-let idade;// variável para idade declarada.
 
-do{
 
-    for(i = 0; i < 3; i++ ){
-        
+for (i = 0; i < 3; i++) {
+    let idade;
+    do {
+        idade = parseFloat(prompt(`Digite a idade ${i+1}`))
+        if (isNaN(idade) || idade < 0 || idade > 100) {
+            alert("Idade inválida, digite novamente");
+        }
+    } while (isNaN(idade) || idade < 0 || idade > 100);
+}
 
-    }
 
-}while()
