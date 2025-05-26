adb11273d06fcd8da73b14de9384eae5f70c482a
unknown <davicustodiosilva36@gmail.com>
2025-05-26 16:18:31 -0300

    feito

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-let nomes = [];
-let alturas = [];
+let nomes = [];// vetor para nomes
+let alturas = [];// vetor para alturas
 let maiorposicao = 0;
 let menorposicao = 0;
 let opcao = [];
 let i = 0;
-let quantidade = 0;
+
 do {
     nomes[i] = prompt(`Digite o ${i + 1}° nome`)
 
     do {
-        alturas[i] = parseInt(prompt(`Digite a altura de ${nomes[i]} :`));
+        alturas[i] = parseFloat(prompt(`Digite a altura de ${nomes[i]} :`));
     } while (isNaN(alturas[i]) || alturas <= 0 || alturas > 3)
     do {
         opcao = prompt(`Digite 0 para continuar ou 1 para finalizar`);
     } while (opcao == "")
     if (opcao != 1) {
-        i++
-        quantidade ++
+        i++;
     }
 
 } while (opcao != 1)// variáveia para a o maior e menor nome e altura
-let maiornome = nomes[0];
+let maiornome = nomes[0];// declaração de variáves para saber o maior e menor nome e altura
 let maioraltura = alturas[0];
 let menornome = nomes[0];
 let menoraltura = alturas[0];
-for (let i = 0; i < quantidade; i++) {
-    
-    if (alturas[i] > maioraltura){
-           maioraltura = alturas[i];
-           maiornome = nomes[i];
-           maiorposicao = i;
-       }
-    
-    if (alturas[i] < menoraltura) {
-           menoraltura = alturas[i];
-           menornome = nomes[i];
-           menorposicao = i
-       }
+for (let j = 0; j <= i; j++) {
+
+    if (alturas[j] > maioraltura) {
+        maioraltura = alturas[j];
+        maiornome = nomes[j];
+        maiorposicao = j;
+      
+    }
+
+    if (alturas[j] < menoraltura) {
+        menoraltura = alturas[j];
+        menornome = nomes[j];
+        menorposicao = j;
+
+    }
 }
 alert(`O nome com maior altura é ${maiornome}, ${maioraltura} metros, na ${maiorposicao}° posição`)
 
 alert(`O nome com menor altura é ${menornome}, ${menoraltura} metros, na ${menorposicao}° posição`)
-
-console.log(nomes, maioraltura, maiornome, maiorposicao, menoraltura, menornome, menorposicao)
