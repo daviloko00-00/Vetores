45d9dfc2b120a6952a4909be080f188be46b4b97
unknown <davicustodiosilva36@gmail.com>
2025-05-26 16:03:39 -0300

    feito

--- index.js
+++ index.js
@@ -1,71 +1,45 @@
-let nomesAlturas = [];// arrawy para armazenar os nomes e alturas
-let contador = 0;// contador para controlar a quantidade de nomes e alturas cadastrados
-let opcao;
-function adicionarnomealtura() {// Função para adicionar nome e altura, fica mais fácil de testes pois cada função fica separado em blocos
+let nomes = [];
+let alturas = [];
+let maiorposicao = 0;
+let menorposicao = 0;
+let opcao = [];
+let i = 0;
+let quantidade = 0;
+do {
+    nomes[i] = prompt(`Digite o ${i + 1}° nome`)
 
-    let nome = prompt("Digite o nome:");
-    let altura = parseFloat(prompt("Digite a altura em metros:"));
-
-    if (isNaN(altura) || altura <= 0 || !(isNaN(nome)) || nome === "") {
-        alert("Altura inválida! Por favor, digite um número positivo.");
-        return;
-    }
-
-    nomesAlturas.push({ nome: nome, altura: altura }); // manda o nome e altura para o array nomesAlturas
-    alert(`Nome e altura adicionados: ${nome}, ${altura}`);
-    console.log(`Nome e altura adicionados: ${nome}, ${altura}`);
-    nomesAlturas[contador] = { nome: nome, altura: altura };// coloca o nome e altura no array nomesAlturas na posição do contador
-    contador++;// incrementa o contador para a próxima posição do array
-    alert(`Total de nomes e alturas cadastrados: ${contador}`);
-    menu()
-}
-function listarnomealtura() {// função para listagem os nomes e alturas cadastrados
-    if (contador === 0) {
-        alert("Nenhum nome e altura cadastrados ainda.");
-        return;
-    }
-
-    let texto = "Lista de nomes e alturas:\n";// variável texto para armazenar a lista de nomes e alturas
-    let maiorAltura = nomesAlturas[0].altura;// variável para armazenar a maior altura, inicia com o primeiro elemento do array
-    let menorAltura = nomesAlturas[0].altura;// variável para armazenar a menor altura, inicia com o primeiro elemento do array
-    let posMenor = 0;// variável para armazenar a posição da menor altura
-    let posMaior = 0;
-
-    for (let i = 0; i < contador; i++) {
-        texto += (i + 1) + ". " + nomesAlturas[i].nome + " - " + nomesAlturas[i].altura + "m\n";
-        if (nomesAlturas[i].altura > maiorAltura) {// verifica se a altura atual é maior que a maior altura
-            maiorAltura = nomesAlturas[i].altura;
-            posMaior = i;
-        } else if (nomesAlturas[i].altura < menorAltura) {// verifica se a altura atual é menor que a menor altura
-            menorAltura = nomesAlturas[i].altura;
-            posMenor = i;
-        }
-    }
-    texto += `\nMaior altura:   ${nomesAlturas[posMaior].nome},  ${maiorAltura}\n Menor altura: ${nomesAlturas[posMenor].nome}, ${menorAltura} `;// exibe as maiores e menores alturas com nomes e posições.
-    alert(texto);
-    console.log(texto);
-    menu();
-}
-
-function menu() {// Função de menu para melhor organização e controle de opções
     do {
-        opcao = parseInt(prompt("Bem-vindo ao sistema de armaxenamento de nomes e alturas!!\n 1. Adicionar nome e altura \n 2. Listar nome e altura. \nSair (0)"));
-        if (isNaN(opcao) || opcao < 0 || opcao > 2) {
-            alert("Opção inválida! Por favor, escolha uma opção válida.");
-        }
-        switch (opcao) {
-            case 1:
-                adicionarnomealtura();
-                break;
-            case 2:
-                listarnomealtura();
-                break;
-            case 0:
-                alert("Saindo...");
-                return;
+        alturas[i] = parseInt(prompt(`Digite a altura de ${nomes[i]} :`));
+    } while (isNaN(alturas[i]) || alturas <= 0 || alturas > 3)
+    do {
+        opcao = prompt(`Digite 0 para continuar ou 1 para finalizar`);
+    } while (opcao == "")
+    if (opcao != 1) {
+        i++
+        quantidade ++
+    }
 
-        }
-    } while (isNaN(opcao) || opcao < 0 || opcao > 2);
+} while (opcao != 1)// variáveia para a o maior e menor nome e altura
+let maiornome = nomes[0];
+let maioraltura = alturas[0];
+let menornome = nomes[0];
+let menoraltura = alturas[0];
+for (let i = 0; i < quantidade; i++) {
+    
+    if (alturas[i] > maioraltura){
+           maioraltura = alturas[i];
+           maiornome = nomes[i];
+           maiorposicao = i;
+       }
+    
+    if (alturas[i] < menoraltura) {
+           menoraltura = alturas[i];
+           menornome = nomes[i];
+           menorposicao = i
+       }
 }
+alert(`O nome com maior altura é ${maiornome}, ${maioraltura} metros, na ${maiorposicao}° posição`)
 
-menu();
+alert(`O nome com menor altura é ${menornome}, ${menoraltura} metros, na ${menorposicao}° posição`)
+
+console.log(nomes, maioraltura, maiornome, maiorposicao, menoraltura, menornome, menorposicao)
